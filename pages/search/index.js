1571b1debc561f4d64e75790a7b98866ea998f54
yaoxin <271958091@qq.com>
2020-12-03 16:30:43 +0800

    小程序完结

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,45 @@
-// pages/search/index.js
+import { request } from '../../request/index'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goods: [],
+    // 取消按钮是否显示
+    isFocus: false,
+    // 输入框的值
+    inputValue: ''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  TimeId: -1,
+  // 输入框的值改变时触发
+  handleInput (e) {
+    clearTimeout(this.TimeId)
+    const {value} = e.detail
+    // trim 去除字符串中的空格
+    // 如果为空 直接 return
+    if (!value.trim()) {
+       this.setData({
+         goods: [],
+         isFocus: false
+       })
+      return
+    }
+    this.setData({
+      isFocus: true
+    })
+    // 如果值不为空 发送请求
+    // 1s后发送请求
+    this.TimeId = setTimeout(() => {
+      request({url: '/goods/qsearch', data: {query: value}}).then(res => {
+        this.setData({
+          goods: res.data.message
+        })
+      })
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击取消按钮
+  handleCancel (e) {
+    this.setData({
+      inputValue: '',
+      isFocus: false,
+      goods: []
+    })
   }
 })
